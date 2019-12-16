d6c13ab7ece20759af772ddf2bba86651dfe1c7b
Rebecca Bartl <rebecca.bartl@gmail.com>
2019-12-16 17:19:21 -0600

    updated models

---
+++ models/book.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    _id: { type: String },
+    title: { type: String, required: true },
+    authors: [{ type: String, required: true }],
+    description: String,
+    image: { type: String, trim: true },
+    link: { type: String, trim: true }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
