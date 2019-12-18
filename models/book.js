3307837983d2cd80a7ab11de97bb77b5dc142edb
Rebecca Bartl <rebecca.bartl@gmail.com>
2019-12-18 17:54:48 -0600

    fixed seed, pages

--- models/book.js
+++ models/book.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
     _id: { type: String },
     title: { type: String, required: true },
-    authors: [{ type: String, required: true }],
+    authors: { type: [String], required: true },
     description: String,
     image: { type: String, trim: true },
     link: { type: String, trim: true }
---
+++ src/pages/Search.js
@@ -0,0 +1,118 @@
+import React, { Component } from "react";
+import Card from "../components/Card";
+import Book from "../components/Book";
+import Jumbotron from "../components/Jumbotron";
+import Form from "../components/Form";
+import Footer from "../components/Footer";
+import { Col, Row, Container } from "../components/Grid";
+import { List } from "../components/List";
+import API from "../utils/API";
+
+class Search extends Component {
+    state = {
+      books: [],
+      q: "",
+      message: "Search For A Book To Begin!"
+    };
+  
+    handleInputChange = event => {
+      const { name, value } = event.target;
+      this.setState({
+        [name]: value
+      });
+    };
+  
+    getBooks = () => {
+      API.getGoogleBooks(this.state.q)
+        .then(res =>
+          this.setState({
+            books: res.data
+          })
+        )
+        .catch(() =>
+          this.setState({
+            books: [],
+            message: "No New Books Found, Try a Different Query"
+          })
+        );
+    };
+  
+    handleFormSubmit = event => {
+      event.preventDefault();
+      this.getGoogleBooks();
+    };
+  
+    handleBookSave = id => {
+      const book = this.state.books.find(book => book.id === id);
+  
+      API.saveBook({
+        googleId: book.id,
+        title: book.volumeInfo.title,
+        subtitle: book.volumeInfo.subtitle,
+        link: book.volumeInfo.infoLink,
+        authors: book.volumeInfo.authors,
+        description: book.volumeInfo.description,
+        image: book.volumeInfo.imageLinks.thumbnail
+      }).then(() => this.getGoogleBooks());
+    };
+  
+    render() {
+      return (
+        <Container>
+          <Row>
+            <Col size="md-12">
+              <Jumbotron>
+                <h1 className="text-center">
+                  <strong>(React) Google Books Search</strong>
+                </h1>
+                <h2 className="text-center">Search for and Save Books of Interest.</h2>
+              </Jumbotron>
+            </Col>
+            <Col size="md-12">
+              <Card title="Book Search" icon="far fa-book">
+                <Form
+                  handleInputChange={this.handleInputChange}
+                  handleFormSubmit={this.handleFormSubmit}
+                  q={this.state.q}
+                />
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col size="md-12">
+              <Card title="Results">
+                {this.state.books.length ? (
+                  <List>
+                    {this.state.books.map(book => (
+                      <Book
+                        key={book.id}
+                        title={book.volumeInfo.title}
+                        subtitle={book.volumeInfo.subtitle}
+                        link={book.volumeInfo.infoLink}
+                        authors={book.volumeInfo.authors.join(", ")}
+                        description={book.volumeInfo.description}
+                        image={book.volumeInfo.imageLinks.thumbnail}
+                        Button={() => (
+                          <button
+                            onClick={() => this.handleBookSave(book.id)}
+                            className="btn btn-primary ml-2"
+                          >
+                            Save
+                          </button>
+                        )}
+                      />
+                    ))}
+                  </List>
+                ) : (
+                  <h2 className="text-center">{this.state.message}</h2>
+                )}
+              </Card>
+            </Col>
+          </Row>
+          <Footer />
+        </Container>
+      );
+    }
+  }
+
+export default Search;
