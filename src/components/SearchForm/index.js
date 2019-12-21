df4c31f1283fd07eb606b595f9af0a0c01d88b07
Rebecca Bartl <rebecca.bartl@gmail.com>
2019-12-21 12:20:37 -0600

    removed fontawesome

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -5,7 +5,7 @@ const SearchForm = props => {
     return (
         <form>
             <div className="form-group">
-                <label className="BookSearchLabel"><h3><i className="fas fa-book" /> Search For A Book</h3></label>
+                <label className="BookSearchLabel"><h3> Search For A Book</h3></label>
                 <br></br>
                 <input className="col-12 form-control"
                     value={props.search}
